b6ce7d66d233a4a4deef171ae6e6b35e287f7c88
Benyamain Yacoob <yacoobbenyamain@gmail.com>
2024-01-14 23:55:27 -0500

    move script finished

--- Assets/Scripts/MoveRepeater.cs
+++ Assets/Scripts/MoveRepeater.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Repeatable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // https://stackoverflow.com/questions/40489102/moving-an-object-with-vector3-movetowards
+    public Vector3 targetPosition = new Vector3(-4f, 0.5f, 2f);
+    public float speed = 1f;
+
+    private Vector3 startPosition;
+    private bool movingForward = true;
+
+    private void Start()
     {
-        
+        // Get the start position
+        startPosition = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called once every frame
+    private void Update()
     {
-        
+        // Change target based on if moving forward or not
+        Vector3 target = movingForward ? targetPosition : startPosition;
+
+        // Move towards the target position
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, target, speed * Time.deltaTime);
+
+        // Check if the object has reached the target position
+        if (transform.localPosition == target)
+        {
+            // Change direction
+            movingForward = !movingForward;
+        }
     }
 }
