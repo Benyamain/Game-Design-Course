17180ff3bdcaa70847152a2c1d13fb24bf1bfde1
Ben Yacoob <yacoobbenyamain@gmail.com>
2024-01-17 19:56:22 -0500

    adding useful comments

--- Assets/Scripts/MoveRepeater.cs
+++ Assets/Scripts/MoveRepeater.cs
@@ -3,33 +3,40 @@ using UnityEngine;
 public class Repeatable : MonoBehaviour
 {
     // https://stackoverflow.com/questions/40489102/moving-an-object-with-vector3-movetowards
-    public Vector3 targetPosition = new Vector3(-4f, 0.5f, 2f);
-    public float speed = 1f;
+    [SerializeField]
+    [Tooltip("The target position of where the game object shall be moving towards in the scene")]
+    private Vector3 _targetPosition = new Vector3(-4f, 0.5f, 2f);
+    [SerializeField]
+    [Tooltip("The rate of movement for the game object")]
+    [Range(0, 10)]
+    private float _speed = 1f;
 
-    private Vector3 startPosition;
-    private bool movingForward = true;
+    [SerializeField]
+    [Tooltip("The start position of where the game object currently lies in the scene")]
+    private Vector3 _startPosition;
+    private bool _movingForward = true;
 
     // Start is called before the first frame update
     private void Start()
     {
         // Get the start position
-        startPosition = transform.localPosition;
+        _startPosition = transform.localPosition;
     }
 
     // Called once every frame
     private void Update()
     {
         // Change target based on if moving forward or not
-        Vector3 target = movingForward ? targetPosition : startPosition;
+        Vector3 target = _movingForward ? _targetPosition : _startPosition;
 
         // Move towards the target position
-        transform.localPosition = Vector3.MoveTowards(transform.localPosition, target, speed * Time.deltaTime);
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, target, _speed * Time.deltaTime);
 
         // Check if the object has reached the target position
         if (transform.localPosition == target)
         {
             // Change direction
-            movingForward = !movingForward;
+            _movingForward = !_movingForward;
         }
     }
 }
--- Assets/Scripts/ScaleRepeater.cs
+++ Assets/Scripts/ScaleRepeater.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class ScaleRepeater : MonoBehaviour
 {
     // Declaration of variables
-    public Vector3 startScale = new Vector3(1f, 1f, 1f);
-    public Vector3 endScale = new Vector3(2f, 2f, 2f);
-    public bool scaleUp = true;
-    public float speed = 1f;
+    [SerializeField]
+    [Tooltip("The original state of the object scale")]
+    private Vector3 _startScale = new Vector3(1f, 1f, 1f);
+    [SerializeField]
+    [Tooltip("The target state of the object scale")]
+    private Vector3 _endScale = new Vector3(2f, 2f, 2f);
+    private bool scaleUp = true;
+    [SerializeField]
+    [Tooltip("The rate of movement for the game object")]
+    [Range(0, 10)]
+    private float _speed = 1f;
 
     // Start is called before the first frame update
     private void Start()
@@ -21,10 +28,10 @@ public class ScaleRepeater : MonoBehaviour
     {
         // Determine the target scale based on the current scale state
         // https://discussions.unity.com/t/resizing-an-object/6145/3
-        Vector3 targetScale = scaleUp ? endScale : startScale;
+        Vector3 targetScale = scaleUp ? _endScale : _startScale;
         
         // Scale towards the target scale
-        transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, speed * Time.deltaTime);
+        transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, _speed * Time.deltaTime);
 
         // Discrepancy of difference accounted for
         // https://forum.unity.com/threads/i-want-to-move-an-object-toward-a-target-and-scale-it-from-1-toward-zero.215055/
